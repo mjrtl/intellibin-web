9c423d0c6a3d5bd19784b93f19710c75b886a154
mjrtl <mjrtlawson@gmail.com>
2023-07-19 13:08:08 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import { Amplify }  from 'aws-amplify'
+import config from './aws-exports'
+import '@aws-amplify/ui-react/styles.css'
+import { AmplifyProvider } from '@aws-amplify/ui-react'
+
+Amplify.configure(config)
+
+const theme = {
+  name: 'intellibin-theme',
+    tokens: {
+    colors: {
+    background: {
+      primary: { value: '#f0d33bff' }
+    }
+  }
+}
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <AmplifyProvider theme = {theme}>
+    <App />
+  </AmplifyProvider>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
